b4aedfda43afcc28459f3dfb8cabb17d09bf1c07
Joshua Behrens <behrens@heptacom.de>
2020-09-06 20:21:45 +0200

    Implement configuration storage
    CONNECT-22

--- src/ConfigurationStorage.php
+++ src/ConfigurationStorage.php
@@ -4,16 +4,51 @@ namespace Heptacom\HeptaConnect\Storage\Native;
 
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\ConfigurationStorageContract;
+use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
+use Heptacom\HeptaConnect\Storage\Native\StorageKey\PortalNodeStorageKey;
 
 class ConfigurationStorage extends ConfigurationStorageContract
 {
+    private FileStorageHandler $storage;
+
+    public function __construct(FileStorageHandler $storage)
+    {
+        $this->storage = $storage;
+    }
+
     public function getConfiguration(PortalNodeKeyInterface $portalNodeKey): array
     {
-        // TODO: Implement getConfiguration() method.
+        if (!$portalNodeKey instanceof PortalNodeStorageKey) {
+            throw new UnsupportedStorageKeyException(\get_class($portalNodeKey));
+        }
+
+        $value = null;
+        $storagePath = $this->getStoragePath($portalNodeKey);
+
+        if ($this->storage->has($storagePath)) {
+            $value = $this->storage->get($storagePath);
+        }
+
+        return \is_null($value) ? $value : (array) \json_decode($value, true);
     }
 
     public function setConfiguration(PortalNodeKeyInterface $portalNodeKey, ?array $data): void
     {
-        // TODO: Implement setConfiguration() method.
+        if (!$portalNodeKey instanceof PortalNodeStorageKey) {
+            throw new UnsupportedStorageKeyException(\get_class($portalNodeKey));
+        }
+
+        $storagePath = $this->getStoragePath($portalNodeKey);
+
+        if (\is_null($data)) {
+            $this->storage->remove($storagePath);
+        }
+
+        $this->storage->put($storagePath, \json_encode($data));
+    }
+
+    private function getStoragePath(PortalNodeStorageKey $portalNodeKey): string
+    {
+        return \sprintf('configuration/%s.json', $portalNodeKey->getId());
     }
 }
